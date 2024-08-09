593a4c48326ef7cff2dd43979dfa91290e3e0dcc
bob <bob@eforest.finance>
2024-08-09 10:26:14 +0800

    chore: upgrade portkey version

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -4,7 +4,7 @@ import { getBasePath } from 'utils/getBasePath';
 let client: ApolloClient<NormalizedCacheObject> | null = null;
 
 const { NEXT_PUBLIC_APP_ENV } = process.env;
-const graphqlUrlPrefix = NEXT_PUBLIC_APP_ENV === 'production' ? 'https://inscription-dapp.eforest.finance' : '';
+const graphqlUrlPrefix = NEXT_PUBLIC_APP_ENV === 'production' ? 'https://inscription-dapp.eforest.finance' : 'https://test-dapp.eforest.finance';
 console.log(graphqlUrlPrefix, 'graphqlUrlPrefix');
 
 export const graphQLClientProvider = (
