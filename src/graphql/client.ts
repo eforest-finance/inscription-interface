6f67f23a206c0762451659c5bc530806c51e74eb
donny-huo <dustin@eforest.finance>
2024-11-28 15:36:20 +0800

    feat: change url

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -4,7 +4,9 @@ let client: ApolloClient<NormalizedCacheObject> | null = null;
 
 const { NEXT_PUBLIC_APP_ENV } = process.env;
 const graphqlUrlPrefix =
-  NEXT_PUBLIC_APP_ENV === 'production' ? 'https://indexer-api.aefinder.io' : 'https://test-indexer-api.aefinder.io';
+  NEXT_PUBLIC_APP_ENV === 'production'
+    ? 'https://indexer-api.aefinder.io/api/app/graphql/inscription'
+    : 'https://test-indexer-api.aefinder.io/api/app/graphql/inscription';
 console.log(graphqlUrlPrefix, 'graphqlUrlPrefix');
 
 export const graphQLClientProvider = (
