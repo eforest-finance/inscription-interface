8b069c49d6feebb6256f9ec2b8aaa14436a5c3ea
donny-huo <dustin@eforest.finance>
2024-12-04 17:48:46 +0800

    feat: icon change

--- src/components/ELFLogo/index.tsx
+++ src/components/ELFLogo/index.tsx
@@ -3,7 +3,7 @@ import TEST_ELF, { ReactComponent as ElfTestComp } from 'assets/images/elf-icon.
 
 const { NEXT_PUBLIC_APP_ENV } = process.env;
 
-const ELF_LOGO = TEST_ELF;
+const ELF_LOGO = NEXT_PUBLIC_APP_ENV === 'production' ? ELF : TEST_ELF;
 const ReactComponent = NEXT_PUBLIC_APP_ENV === 'production' ? ElfComp : ElfTestComp;
 
 export default ELF_LOGO;
